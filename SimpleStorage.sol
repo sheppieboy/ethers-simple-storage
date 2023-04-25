3599960466a55f778e4cd7ee93d36fec20f3c156
Luke Sheppard <sheppiegit@gmail.com>
2023-04-25 11:33:03 -0400

    Added npm packages

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -16,13 +16,12 @@ contract SimpleStorage {
         favouriteNumber = _favouriteNumber;
     }
 
-    function retrieve() public view returns(uint256){
+    function retrieve() public view returns (uint256) {
         return favouriteNumber;
     }
 
-    function addPerson(string memory _name, uint256 _favoriteNumber) public{
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
         people.push(People(_favoriteNumber, _name));
         nameToFavoriteNumber[_name] = _favoriteNumber;
     }
-
 }
